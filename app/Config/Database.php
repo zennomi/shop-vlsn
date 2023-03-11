5c6b686d478d90095346e138dd96b4697a980ef1
zen-sv-2 <zennomi.kun@gmail.com>
2023-03-10 22:10:32 -0500

    initv2

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'username' => 'modesy',
+		'password' => 'password',
+		'database' => 'modesy',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
